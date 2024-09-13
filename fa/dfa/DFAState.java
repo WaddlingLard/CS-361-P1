283fc5de234ca3bf3ae41f4876f13be36d39b182
Brian Wu <brianwu@u.boisestate.edu>
2024-09-13 12:30:53 -0600

    slight changes on DFAState. working on a method to store states

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -2,7 +2,12 @@ package fa.dfa;
 
 import fa.State;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DFAState extends State {
+
+    private Map<Character, DFAState> transitions;
 
     public DFAState() {
 
@@ -10,6 +15,17 @@ public class DFAState extends State {
 
     public DFAState(String name) {
         super(name); // Calling the super constructor on the State abstract class
+        transitions = new HashMap<>();
+    }
+
+    /*
+
+     */
+    public boolean addDFATransition(Character sigma, DFA location) {
+
+
+
+        return false;
     }
 
 }
